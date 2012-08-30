3fa4e6ac6d4f10b3ec39c18b841fcc22de2492bb
goldbo <549668352@qq.com>
2012-08-30 23:32:08 +0800

    nothing

--- test/info/goldbo/base/SpringTest.java
+++ test/info/goldbo/base/SpringTest.java
@@ -3,6 +3,11 @@ package info.goldbo.base;
 import info.goldbo.base.entity.Admin;
 import info.goldbo.base.service.AdminService;
 
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -42,15 +47,41 @@ public class SpringTest  {
 		admin.setUsername("huang");
 		admin.setPassword("123456");
 		adminService.save(admin);
+		System.out.println(admin.getId());
 	}
 	
 	@Test
 	public void testUpdate(){
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		AdminService adminService = (AdminService) context.getBean("adminServiceImpl");
-		Admin admin = adminService.load("402881d6390fc05e01390fc0636a0001");
+		Admin admin = adminService.load("297e0cc1390b92a301390b92ac6c0001");
 		//System.out.println(admin.getId());
 		admin.setPassword("123456");
 		//adminService.update(admin);
+	}
+	
+	@Test
+	public void testSession(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		SessionFactory sf = (SessionFactory)context.getBean("sessionFactory");
+		Session session = sf.openSession();
+		session.beginTransaction();
+		Admin admin = (Admin)session.load(Admin.class, "297e0cc1390b92a301390b92ac6c0001");
+		admin.setEmail("admin@qq.com");
+		session.getTransaction().commit();
+		session.close();
+	}
+	
+	@Test
+	public void testList(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		AdminService adminService = (AdminService) context.getBean("adminServiceImpl");
+		List<Admin> list = adminService.getAll();
+		if(list!=null){
+			for (Admin admin : list) {
+				admin.setDepartment("test_department");
+				adminService.update(admin);
+			}
+		}
 	}
 }
